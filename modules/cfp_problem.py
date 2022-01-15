4f6f3cec1a2b091e2f6229a52ec9528aa31e8729
Nathan Tolbert <ntolbertu85@gmail.com>
2022-01-15 07:53:46 -0500

    01152021_--_cfp_problem.py_--_Add Problem class
      - added initial dummy values to declarations for properties
      - added property 'solved_by_user'
      - added mark_solved() func for setting it to True

--- modules/cfp_problem.py
+++ modules/cfp_problem.py
@@ -1,13 +1,14 @@
 class Problem:
-    contestId: int = -1     # Can be absent. Id of the contest, containing the problem.
-    problemsetName: string  # Can be absent. Short name of the problemset the problem belongs to.
-    index: string           # Usually, a letter or letter with digit(s) indicating the problem index in a contest.
-    name: string            # Localized.
-    problem_type: Enum      # PROGRAMMING, QUESTION.
-    points: float           # Can be absent. Maximum amount of points for the problem.
-    rating: int = -1        # Can be absent. Problem rating (difficulty).
-    tags: list[string]      # Problem tags.
-
+    contestId: int = -1       # Can be absent. Id of the contest, containing the problem.
+    problemsetName: string = ''    # Can be absent. Short name of the problemset the problem belongs to.
+    index: string = ''        # Usually, a letter or letter with digit(s) indicating the problem index in a contest.
+    name: string = ''         # Localized.
+    problem_type: string = '' # PROGRAMMING, QUESTION.
+    points: float = float(-1) # Can be absent. Maximum amount of points for the problem.
+    rating: int = -1          # Can be absent. Problem rating (difficulty).
+    tags: list[string] = list()       # Problem tags.
+    solved_by_user: bool = False
+    
     def __init__(contestId: int, problemsetName: string, index: string, name: string, problemType: string, points: float, rating: int, tags: list[string]):
         self.contestId = contestId 
         self.problemset_name = problemsetName
@@ -20,4 +21,13 @@ class Problem:
         self.points = float(points)
         self.rating = rating
         self.tags = tags
+        return True
+    
+    def mark_solved(self):
+        self.solved_by_user = True
+        return True
+        
+    def mark_unsolved(self):
+        self.solved_by_user = False
+        return True
         
