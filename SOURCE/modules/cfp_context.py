76992e484230bfdc38f6ce576d60c45daf730502
Nathan Tolbert <ntolbertu85@gmail.com>
2022-01-31 15:27:53 -0500

    01312022_--_cfp_context_--_minor_changes
      - Added import for click
      - Changed The in-comment visualization for
        context class
            - Changed the right-pointing arrows for has-a relationships
              to a left-pointing arrows
      - Context base class
          - Added dataclass decorator at the class level
          - Removed an unfinished sentence fragment from docstring.
      - Cfp_Shell_Context class
          - Added declaration reference to extend the Context class
          - Added info to the class docstring
          - Changed the name of the `progs' list to `cmds`
          - Added variable `shell'
          - Fixed indentation on the __init__ docstring
          - Added declarations for 2 new functions
              - check_for_preferred_shell()
              - __run_command()
      - Added class
          - DynamicStrRunnerContext(Context)
          - Added declaration and docstring

--- SOURCE/modules/cfp_context.py
+++ SOURCE/modules/cfp_context.py
@@ -1,4 +1,4 @@
-import os
+import os, click
 
 
 
@@ -6,16 +6,16 @@ import os
 #          ^
 #          ^
 #          |
-#  has-a = |   /  is-a =  ---- > > >
+#  has-a = |   /  is-a =  < < < ----
 
-#                 context ---------------------------- > > >  test_contest
+#                 context < < < ----------------------------  test_contest
 #                  |   |
 #                 |     |
 #                |       |
 #               |         |
 #              |           |
-#     
-#     
+#        environment     
+#    
 #     
 #     
 #     
@@ -28,19 +28,27 @@ import os
 #     
 #     
 #
+
+
+@dataclass
 class Context:
     '''
-    Base for all contexts. Main purpose is just to act
+    Base for all contexts. 
     '''
+    namespace: str = ''
+    ctx_type: str = ''
+    
+
 
      
 
-class Cfp_Shell_Context:
+class Cfp_Shell_Context(Context):
     '''
-    This is a context which is run on top of a process with its own environment that is kept seperate from the process environment by default, but whose variables can be accessed in the same way as process envvars at context runtime.
+    This is a context for running commands in a shell such as bash or zsh. The bash process is run on top of a Python process with its own environment that is kept seperate from the process environment by default, but whose variables can be accessed in the same way as process envvars at context runtime.
     '''
     env_dict = dict()
-    progs = []
+    shell = ''
+    cmds:list[Command] = []
     cf_allowedlangs = ['C# mono',
                         'D DMD32',
                         'Go',
@@ -65,19 +73,22 @@ class Cfp_Shell_Context:
                         'JavaScriptV8'
                         'nodejs'
                        ]
+    # represents the chosen language's index in the cf_allowedlangs list 
     cf_lang_index = -1   
     
     def __init__(self, cmds, **envvars):
         '''
-        
+        Init calls parent init (sets namespace, ctx_type) and updates virtual_environment 
         '''
-        env_dict.update(envvars)
+        super().__init__('shell_ctx','shell')
+        
+        self.env_dict.update(envvars)
         if type(cmds) == list:
             if type(cmds[0]) == list:
+                pass
         elif type(cmds) == str:
-            cmds_ls = cmds.split('|')
-            for c in cmds_ls:
-                c.
+            pass
+            
     def putenv(k, v):
         env_dict.update({k: v})
         return True
@@ -92,7 +103,19 @@ class Cfp_Shell_Context:
     def getenv(self, key):
         return self.env_dict[key]
     
+    def check_for_preferred_shell(self, shellpref:string):
+        '''runs which command with shellname as argument. If cmd returns empty, self.shellpref_avail is set to False and this func returns False. otherwise,it is set to True, and func returns the path which the os uses to execute it, usually "$PREFIX/bin/shellname".'''
+        
     
+    def __run_commands(self,cmd: str, shellpath):
+        '''simply runs cmd using self.shellpref. self.shellpref_avail must be True. DO NOT SET IT YOURSELF! To set it, you must first run the check_for_preferred_shell() func above. If it is False, then the shell isn't installed on the current system. In this case '''
+        cmds_ls = cmds.split('&&')
+        for c in cmds_ls:
+            pass
+            
+                
+class DynamicStrRunnerContext(Context): 
+    '''sets up the runner based on the value of ctx_type in the parent. Uses concept known as reflection in Java via running eval(runner_str) where runner str is based on ctx_type. This lets us dynamically build a string and then run that string as python3 code. e.g. say ctx_type is "subprocess". The resulting runner_str would be "subprocess.run(cmd)". '''
     
 class CfpTestContext(CfpContext):
 
