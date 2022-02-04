38274a69ee16615690ef8b45a4693d95b0bfceb2
Nathan Tolbert <ntolbertu85@gmail.com>
2022-02-04 15:31:36 -0500

    02042022_--_cfp_context_--_move_lang_to_CfpTestCtx
      - made changes to cfp_context.py
          - class CfpShellContext:
              - added property cmds_fmt
                  - represents cmds after formatting: Type list[list]
             - added check to ensure that cmds are Type list[list]
                  - properly formatted cmds are added to cmds_fmt
                  - unformatted cmds are formatted before adding
            - removed setlang() func (moved)
        - class CfpTestContext:
            - added cf_allowedlangs and cf_lang_index properties
            - added setlang() function

--- SOURCE/modules/cfp_context.py
+++ SOURCE/modules/cfp_context.py
@@ -45,8 +45,50 @@ class CfpShellContext(Context):
     This is a context for running commands in a shell such as bash or zsh. The bash process is run on top of a Python process with its own environment that is kept seperate from the process environment by default, but whose variables can be accessed in the same way as process envvars at context runtime.
     '''
     
-    shell:str = ''
+    shell: str = ''
     cmds: list = []
+    
+     
+    
+    def __init__(self, cmds, **envvars):
+        '''
+        Init calls parent init (sets namespace, ctx_type) and updates virtual_environment. Sets `cmds_fmt` to a 2d list where each outer element represents a command, itself represented by the inner list, with cmd[0] being the command and the rest of the inner list is its args. 
+        '''
+        super().__init__('shell_ctx','shell')
+        
+        self.env_dict.update(envvars)
+        self.cmds_fmt = list()
+        for cmd in cmds:
+            if type(cmd) == list:
+                self.cmds_fmt.append(cmd)
+            elif type(cmd) == str:
+                self.cmds_fmt.append(list(map(str, cmd.split(' '))))
+
+        
+                
+    def putenv(k, v):
+        env_dict.update({k: v})
+        return True
+            
+    def getenv(self, key):
+        return self.env_dict[key]
+    
+    def check_for_preferred_shell(self, shellpref:str):
+        '''runs which command with shellname as argument. If cmd returns empty, self.shellpref_avail is set to False and this func returns False. otherwise,it is set to True, and func returns the path which the os uses to execute it, usually "$PREFIX/bin/shellname".'''
+        
+    
+    def __run_commands(self, cmds: str, shellpath):
+        '''simply runs cmd using self.shellpref. self.shellpref_avail must be True. DO NOT SET IT YOURSELF! To set it, you must first run the check_for_preferred_shell() func above. If it is False, then the shell isn't installed on the current system. In this case '''
+        cmds_ls = cmds.split('&&')
+        for c in cmds_ls:
+            pass
+            
+                
+class DynamicStrRunnerContext(Context): 
+    '''sets up the runner based on the value of ctx_type in the parent. Uses concept known as reflection in Java via running eval(runner_str) where runner str is based on ctx_type. This lets us dynamically build a string and then run that string as python3 code. e.g. say ctx_type is "subprocess". The resulting runner_str would be "subprocess.run(cmd)". '''
+    
+class CfpTestContext(CfpShellContext):
+
     cf_allowedlangs = ['C# mono',
                         'D DMD32',
                         'Go',
@@ -68,56 +110,23 @@ class CfpShellContext(Context):
                         'Ruby',
                         'Rust',
                         'Scala',
-                        'JavaScriptV8'
+                        'JavaScriptV8',
                         'nodejs'
                        ]
-    # represents the chosen language's index in the cf_allowedlangs list 
-    cf_lang_index = -1   
     
-    def __init__(self, cmds, **envvars):
-        '''
-        Init calls parent init (sets namespace, ctx_type) and updates virtual_environment 
-        '''
-        super().__init__('shell_ctx','shell')
-        
-        self.env_dict.update(envvars)
-        if type(cmds) == list:
-            if type(cmds[0]) == list:
-                pass
-        elif type(cmds) == str:
-            pass
-            
-    def putenv(k, v):
-        env_dict.update({k: v})
-        return True
+    # represents the chosen language's index in the cf_allowedlangs list 
+    cf_lang_index = -1 
+
+    def __init__(self, cmds, language, **envvars):
+        super().__init__(cmds, envvars)
+        self.setlang(language)
     
     def setlang(self):
+        for i,lang_choice in enumerate(self.cf_allowedlangs):
+            if lang_choice.lower() in ''.join(list(map(str, language.split(' ')))).lower():
+                self.cf_lang_index = i
         if cf_lang_index < 0:
             raise IOError('You must provide a language!')
-        else:
+        else:    
             lang = self.cf_allowedlangs[cf_lang_index]
             self.putenv('solutionlanguage', lang)
-            
-    def getenv(self, key):
-        return self.env_dict[key]
-    
-    def check_for_preferred_shell(self, shellpref:str):
-        '''runs which command with shellname as argument. If cmd returns empty, self.shellpref_avail is set to False and this func returns False. otherwise,it is set to True, and func returns the path which the os uses to execute it, usually "$PREFIX/bin/shellname".'''
-        
-    
-    def __run_commands(self, cmds: str, shellpath):
-        '''simply runs cmd using self.shellpref. self.shellpref_avail must be True. DO NOT SET IT YOURSELF! To set it, you must first run the check_for_preferred_shell() func above. If it is False, then the shell isn't installed on the current system. In this case '''
-        cmds_ls = cmds.split('&&')
-        for c in cmds_ls:
-            pass
-            
-                
-class DynamicStrRunnerContext(Context): 
-    '''sets up the runner based on the value of ctx_type in the parent. Uses concept known as reflection in Java via running eval(runner_str) where runner str is based on ctx_type. This lets us dynamically build a string and then run that string as python3 code. e.g. say ctx_type is "subprocess". The resulting runner_str would be "subprocess.run(cmd)". '''
-    
-class CfpTestContext(CfpShellContext):
-
-
-    def __init__(self, cmds, **envvars):
-        super().__init__(cmds, envvars)
-         
