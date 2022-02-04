f16f9812f3bebba76d9f9e25be9b6c20ac39eb6d
Nathan Tolbert <ntolbertu85@gmail.com>
2022-02-04 07:58:53 -0500

    02042022_--_project-wide_--_misc_changes
      - made changes to config to enable pytest
          - added __init__.py to all needed dirs
          - added top-lvl main.py dummy script
          - renamed old setup.py & added new
          - various syntax changes to test script

--- TEST/test_context_classes.py
+++ TEST/test_context_classes.py
@@ -1,17 +1,17 @@
-from ...sourcepy.modules.cfp_context import *
+from cfpipeline.SOURCE.modules.cfp_context import *
 
 def test__Context_base_init_01():
     ctx = Context("TEST_CTX_", "example", {})
-    assert ctx.namespace == "TEST_CTX" 
+    assert ctx.namespace == "TEST_CTX_" 
     
 def test__Context_base_init_02():
     ctx = Context("TEST_CTX_", "example", {"wrd1":"Hello ", "wrd2":"world!"})
     assert ctx.env_dict["wrd1"] == "Hello " 
     
-def test__Context_base_init_01():
+def test__Context_base_init_03():
     ctx = Context("TEST_CTX_", "example", {})
     assert ctx.ctx_type =="example"
     
-def test_Context_base_init_02():
+def test_Context_base_init_04():
     ctx = Context("TEST_CTX_", "example", {"wrd1":"Hello ", "wrd2":"world!"})
     assert ctx.env_dict["wrd2"] == "world!"
