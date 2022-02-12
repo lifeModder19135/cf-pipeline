d6d3ec718aedfda6e60fec4d859ed216d694e796
Nathan Tolbert <ntolbertu85@gmail.com>
2022-02-12 06:31:19 -0500

    02122022_--_test_ctx_base_class_--_minor_change
      - in each method, add description  variable
      - ...init_01()
          - remove description() func
      - ALL TESTS PASSING

--- TEST/test_context_base_class.py
+++ TEST/test_context_base_class.py
@@ -1,19 +1,22 @@
 from cfpipeline.SOURCE.modules.cfp_context import *
 
 def test__Context_base_init_01():
-    description("ensure that new Context objs are created with")
+    description = "ensure that new Context objs are created with namespace set"
     ctx = Context("TEST_CTX_", "example", {})
     assert ctx.namespace == "TEST_CTX_" 
     
 def test__Context_base_init_02():
+    description = "ensure that new Context objs are created with populated env_dict - 1"
     ctx = Context("TEST_CTX_", "example", {"wrd1":"Hello ", "wrd2":"world!"})
     assert ctx.env_dict["wrd1"] == "Hello " 
     
 def test__Context_base_init_03():
+    description = "ensure that new Context objs are created with ctx_type set"
     ctx = Context("TEST_CTX_", "example", {})
     assert ctx.ctx_type =="example"
     
 def test_Context_base_init_04():
+    description = "ensure that new Context objs are created with populated env_dict - 2"
     ctx = Context("TEST_CTX_", "example", {"wrd1":"Hello ", "wrd2":"world!"})
     assert ctx.env_dict["wrd2"] == "world!"
     
