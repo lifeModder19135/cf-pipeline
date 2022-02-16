dc5e115767f19c40e06515b9ad1d2521318fed3c
Nathan Tolbert <ntolbertu85@gmail.com>
2022-02-16 01:50:46 -0500

    02162022_--_libcf-api/*_--_added_lib_files
      -  Added libcf-api library / dir to '''/SOURCE/lib/
          - Added contained files:
              - libcfapi-method_objs.py
              - libcfapi-constants
              - libcfapi_utils.py
      - Added tag-related methods to libcfp_meta...

--- SOURCE/lib/libcfp_metautils.py
+++ SOURCE/lib/libcfp_metautils.py
@@ -1,4 +1,20 @@
+from os import 
+
 class Option(str):
     """
     Type for a kwarg string in a python functions t
     """
+    
+class TagLocker(dict):
+    """
+    
+    Args:
+        dict (_type_): _description_
+    """
+    
+class TagTools:
+    
+    def __init__():
+        pass
+    
+    get
