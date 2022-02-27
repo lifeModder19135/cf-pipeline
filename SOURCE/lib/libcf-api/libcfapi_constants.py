cfb91aa50efd393602f24a03d8509a80865e1be0
Nathan Tolbert <ntolbertu85@gmail.com>
2022-02-26 20:17:56 -0500

    02222022_--_.gitignore_--_added-file\n\n  - file for this development environment only

--- SOURCE/lib/libcf-api/libcfapi_constants.py
+++ SOURCE/lib/libcf-api/libcfapi_constants.py
@@ -1,2 +1,9 @@
+from invoke import run
 CFAPI_NAMESPACE = 'CFAPI_'
-CFAPI_PROBSLIST_INIT= 'wget https://codeforces.com/api/problemset.problems && cat ./problemset.problems | sed "s/},{/},\n{/g" | tee ./cf_probslist.json | less'
+CFP_CONTEXT_DIR = '~/.local/lib/cfpipeline-context'
+CFAPI_DATA_DIR = 
+CFAPI_DATA_SUBDIR = 'problemset'
+CFAPI_PROBSLIST_DEFAULT = '/'.join(CFAPI_DATA_DIR, CFAPI_DATA_SUBDIR, 'problemset.problems')
+CFAPI_PROBSLIST__OBJ_PERLINE = '/cf_probslist.json/'
+
+CFAPI_PROBSLIST_INIT= run(f'wget https://codeforces.com/api/problemset.problems && cat {} | sed "s/},{/},\n{/g" | tee . | less')
