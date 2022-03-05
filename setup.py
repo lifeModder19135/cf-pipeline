db7f4dce9581c3516c9cc7f459e7bc02d21b11fa
Nathan Tolbert <ntolbertu85@gmail.com>
2022-03-05 09:16:47 -0500

    03052022_--_project_--_fix_editor_gui
      - vs code git gui editor is showing setup.py mods as uncommited,
      - commiting to satisfy

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name = 'cfpipeline', version = '0.0.1', packages = find_packages())
+setup(
+    name = 'cfpipeline', 
+    version = '0.0.1', 
+    packages = ['cfpipeline', 'cfp_commands', 'cfp_abstract'],
+    package_dir = {'cfpipeline': 'SOURCE', 'cfp_commands': 'SOURCE/commands', 'cfp_abstract': 'SOURCE/modules'},
+    install_requires = [
+        'click',
+        'poetry',
+        'invoke',
+        'pytest',
+        'ward'
+    ],
+    entry_points = '''
+        [console_scripts]
+        cfp=cfpipeline 
+    '''
+    )
