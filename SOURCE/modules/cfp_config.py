e54b4fbc673183652c4225306e26e868b169485c
Nathan Tolbert <ntolbertu85@gmail.com>
2022-03-21 09:47:38 -0400

    03212022_--_from_stash

--- SOURCE/modules/cfp_config.py
+++ SOURCE/modules/cfp_config.py
@@ -1,13 +1,21 @@
 import os, typing
 from ..lib import libcfapi_utils
+<<<<<<< Updated upstream
 from .cfp_errors import CfpInitializationError, CfpUserInputError, CfpOverwriteNotAllowedError
+=======
+from .cfp_errors import CfpInitializationError, CfpTypeError, CfpUserInputError, CfpOverwriteNotAllowedError
+>>>>>>> Stashed changes
 from enum import Enum
 
 class AppConfigurationOptions(Enum):
     '''
     Allowed config options. The names correspond to the options allowed as l_values in the conffile. the values are stringified representations
     '''
+<<<<<<< Updated upstream
 class Section:
+=======
+class ConfFileSection:
+>>>>>>> Stashed changes
     pass
 
 class ConfigFile(object):
@@ -27,7 +35,14 @@ class ConfigFile(object):
             self.__sectslist_ = []
         if action == 'update':
             for a in args:
+<<<<<<< Updated upstream
                 self.__sectslist_.append(a)
+=======
+                if type(a) == ConfFileSection:
+                    self.__sectslist_.append(a)
+                else:
+                    raise CfpTypeError()
+>>>>>>> Stashed changes
         elif action == 'overwrite':
             self.__sectslist_ = []
             for a in args:
@@ -37,9 +52,37 @@ class ConfigFile(object):
         elif action == 'refresh':
             self.__get_sections_from_conffile()
 
+<<<<<<< Updated upstream
     def __get_sections_from_conffile(self):
         _filelocation = '/'.join(self.location_path(),self.filename())
         with open(_filelocation, 'r')    
+=======
+    @property
+    def location_dirpath(self) -> str:
+        return self.__locdirpath
+
+    @location_dirpath.setter
+    def location_dirpath(self, lp) -> None:
+        self.__locdirpath = lp
+
+    @property
+    def filename(self) -> str:
+        return self.__file_name
+
+    @filename.setter
+    def filename(self, fname) -> None:
+        self.__file_name = fname
+
+    def __get_sections_from_conffile(self):
+        _filelocation = '/'.join(self.location_path(),self.filename())
+        with open(_filelocation, 'r') as cfgfile:
+            for line in cfgfile:
+                if str(line).isspace:
+                    continue
+        
+
+    def __parse_conffile(self):
+>>>>>>> Stashed changes
 
 class AppConfiguration(typing.dict):
     '''
@@ -59,10 +102,19 @@ class AppConfiguration(typing.dict):
                         pass
                         # TODO: ^^
             if iter:
+<<<<<<< Updated upstream
                 if type(iter) == list:
 
 
             super().__init__(conf_or_iter, **kvpairs)
+=======
+                if type(iter) == list or type(iter) == tuple:
+                    for node in iter:
+                        if type(node) != list and type(node) != tuple:
+                            raise CfpTypeError()
+
+            super().__init__(conf_dict, iter, **kvpairs)
+>>>>>>> Stashed changes
 
     def __init__(self, **kvpairs):
         super().__init__(**kvpairs)
