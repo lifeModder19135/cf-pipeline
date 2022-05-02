d068c4a750f3989fc7be0cc333de21be141bf644
Nathan Tolbert <ntolbertu85@gmail.com>
2022-05-02 00:18:12 -0400

    05012022_--_cfp_test_context_--_misc_changes
      - added imports:
          - CfpFile
          - CfpRunner
      - simple_tester:
          - fixed Parent class declaration
          - method setup_pipes:
              - added params
                  - moved from class declaration
                  - added type annotations
              - TODO:
                  - FINISH ME!!!!!!

--- SOURCE/modules/cfp_test_context.py
+++ SOURCE/modules/cfp_test_context.py
@@ -1,7 +1,8 @@
 import os, subprocess
+from .cfp_context import CfpFile, CfpRunner
 
-class simple_tester(lang,input_file,source_file):
+class simple_tester(CfpRunner):
     
-    def setup_pipes():
+    def setup_pipes(lang,input_file:CfpFile,source_file:CfpFile):
         ps1 = subprocess.run(input=subprocess.STDIN, output=subprocess.PIPE)
 
