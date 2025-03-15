92f8d812109fa8fed23a777076b1df617686f319
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-15 14:36:41 -0400

    03092025_--_cfp_config_test_--_added_to_method:   cfp_config_test.py:     - added assert statements to method create_conffilesection_test()

--- TEST/modules_test/cfp_config_test.py
+++ TEST/modules_test/cfp_config_test.py
@@ -4,3 +4,6 @@ def create_conffilesection_test():
 
     section = ConfFileSection('test section', 'a test section', Action.UPDATE, {'test_key': 'test_value'})
     assert type(section) == ConfFileSection
+    assert section.name == 'test section'
+    assert section.description == 'a test section'
+    assert type(section.keys_vals_dict) == dict
