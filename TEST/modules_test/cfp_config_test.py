2f3c1061770e0eb627a787506814c87c83af2e1e
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-18 13:56:27 -0400

    03182025_--_cfp_config_test_--_added_test:  cfp_config_test.py:     - added test for ConfigFile creation

--- TEST/modules_test/cfp_config_test.py
+++ TEST/modules_test/cfp_config_test.py
@@ -7,3 +7,12 @@ def test_create_conffilesection_test():
     assert section.name == 'test section'
     assert section.description == 'a test section'
     assert type(section.keys_vals_dict) == dict
+
+def test_create_ConfigFile_test():
+    section_1 = ConfFileSection('test section 1', 'a test section', Action.UPDATE, {'test_key': 'test_value'})
+    section_2 = ConfFileSection('test section 2', 'a test section', Action.UPDATE, {'test_key': 'test_value'})
+    file = ConfigFile('/test/path', 'test.py', [Action.UPDATE, section_1, section_2])
+    assert file.location_dirpath == '/test/path'
+    assert file.filename == 'test.py'
+    assert type(file.sections[0]) == ConfFileSection
+    assert type(file.sections[1]) == ConfFileSection
