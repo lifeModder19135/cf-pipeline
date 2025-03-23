e76b17c8a2f96c7c967dcb2b7b6de0a2b5952c3b
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-23 18:23:00 -0400

    03202025_--_cfp_contest_test_--_added_test:    cfp_contest_test.py:      - uncommented  method      - added test test_create_iohandlerbase_withstring_test()      - added test test_create_inputhandler_test()

--- TEST/modules_test/cfp_context_test.py
+++ TEST/modules_test/cfp_context_test.py
@@ -1,9 +1,17 @@
 from SOURCE.modules.cfp_context import IOHandlerBase, IOType
 
 
-# def test_create_iohandlerbase_test():
-#     base = IOHandlerBase(['test value 1', 'test value 2'], IOType.INPUT)
-#     print(base.handler_args)
-#     print(type(base.io_type))
-#     assert base.io_type == IOType.INPUT
-#     assert base.handler_args == 'test_key=test value'
+def test_create_iohandlerbase_test():
+    base = IOHandlerBase(['test value 1', 'test value 2'], IOType.INPUT)
+    assert base.io_type == IOType.INPUT
+    assert type(base.handler_args) == list
+    assert base.handler_args[0] == 'test value 1'
+
+def test_create_iohandlerbase_withstring_test():
+    base = IOHandlerBase(['test value 1', 'test value 2'], 'i')
+    assert base.io_type == IOType.INPUT
+    assert type(base.handler_args) == list
+    assert base.handler_args[0] == 'test value 1'  
+
+def test_create_inputhandler_test():
+    pass
