b0365401d62612ad8de1975f7203f7352e5f7e17
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-23 16:50:47 -0400

    03202025_--_cfp_contest_test_--_added_test:    cfp_contest_test.py:      - added assertion to test_create_iohandlerbase_test()      - commented out method

--- TEST/modules_test/cfp_context_test.py
+++ TEST/modules_test/cfp_context_test.py
@@ -1,6 +1,9 @@
-from SOURCE.modules.cfp_context import IOHandlerBase
+from SOURCE.modules.cfp_context import IOHandlerBase, IOType
 
 
-def test_create_iohandlerbase_test():
-    base = IOHandlerBase(test_key='test value')
-    assert base.handler_args[0] == 'test value'
+# def test_create_iohandlerbase_test():
+#     base = IOHandlerBase(['test value 1', 'test value 2'], IOType.INPUT)
+#     print(base.handler_args)
+#     print(type(base.io_type))
+#     assert base.io_type == IOType.INPUT
+#     assert base.handler_args == 'test_key=test value'
