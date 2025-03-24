fe98b330a1eb78c7d336ae7add1e276b9c747177
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-23 23:39:01 -0400

    03202025_--_cfp_contest_test_--_added_test:    cfp_contest_test.py:      - added imports for PosixPath, WindowsPath, CfpFile, and FileType      - added seperators for tests of seperate classes      - added test  test_create_inputhandler_fails_properly_test()      - added test test_create_cfpfile_test()      - added test test_create_inputfilehandler_test()

--- TEST/modules_test/cfp_context_test.py
+++ TEST/modules_test/cfp_context_test.py
@@ -1,7 +1,8 @@
-from SOURCE.modules.cfp_context import IOHandlerBase, IOType, InputHandler, InputType
+from SOURCE.modules.cfp_context import IOHandlerBase, IOType, InputHandler, InputType, CfpFile, FileType
 import pytest
 from SOURCE.modules.cfp_errors import CfpInitializationError, CfpMethodInputError, CfpTypeError, CfpValueError, CfpUserInputError
-
+from pathlib import Path, PosixPath
+########################################  ~~~~ IOHandlerBase ~~~~  #####################
 
 def test_create_iohandlerbase_test():
     base = IOHandlerBase(['test value 1', 'test value 2'], IOType.INPUT)
@@ -24,8 +25,33 @@ def test_create_iohandlerbase_fails_properly_test():
         base = IOHandlerBase(['test value 1', 'test value 2'], 14)
     # assert str(e.value) == 'The value provided for io_type must be of type string or IOType.'
 
+########################################  ~~~~ InputHandler ~~~~  #####################
+
 def test_create_inputhandler_test():
     hndlr = InputHandler(InputType.INFILE, ['test value 1', 'test value 2'])
     assert hndlr.input_type == InputType.INFILE
     assert hndlr.io_type == IOType.INPUT
     assert hndlr.handler_args[1] == 'test value 2'
+
+def test_create_inputhandler_fails_properly_test():
+     with pytest.raises((CfpTypeError, CfpUserInputError)):
+         hndlr = InputHandler('wrong type', ['test value 1', 'test value 2'])
+
+########################################  ~~~~ CfpFile ~~~~  #####################
+
+def test_create_cfpfile_test():
+    hndlr = InputHandler(InputType.INFILE, ['test value 1', 'test value 2'])
+    path = Path('/test/path/testfile.txt')
+    file = CfpFile(hndlr, path, FileType.CFP_INPUTFILE_TEXT_FMT_1, 100)
+    assert type(file.handler) == InputHandler
+    assert type(file.location_path) == PosixPath
+    assert file.filetype == FileType.CFP_INPUTFILE_TEXT_FMT_1
+    assert file.is_openable == False
+
+def test_create_inputfilehandler_test():
+    hndlr = InputHandler(InputType.INFILE, ['test value 1', 'test value 2'])
+    path1 = Path('/test/path/testfile1.txt')
+    path2 = Path('/test/path/testfile2.txt')
+    file1 = CfpFile(hndlr, path1, FileType.CFP_INPUTFILE_TEXT_FMT_1, 100)
+    file2 = CfpFile(hndlr, path2, FileType.CFP_INPUTFILE_TEXT_FMT_1, 100)
+ 
