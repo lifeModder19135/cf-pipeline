79b9ce045591c478e374bdb9fab15ad6f9db0acf
Nathan Tolbert <ntolbertu85@gmail.com>
2025-03-23 20:55:39 -0400

    03202025_--_cfp_contest_test_--_added_test:    cfp_contest_test.py:      - added imports for errors and pytest      - added logic for test_create_inputhandler_test()      - added test test_create_iohandlerbase_fails_properly_test() to ensure proper errors are raised

--- TEST/modules_test/cfp_context_test.py
+++ TEST/modules_test/cfp_context_test.py
@@ -1,4 +1,6 @@
-from SOURCE.modules.cfp_context import IOHandlerBase, IOType
+from SOURCE.modules.cfp_context import IOHandlerBase, IOType, InputHandler, InputType
+import pytest
+from SOURCE.modules.cfp_errors import CfpInitializationError, CfpMethodInputError, CfpTypeError, CfpValueError, CfpUserInputError
 
 
 def test_create_iohandlerbase_test():
@@ -11,7 +13,19 @@ def test_create_iohandlerbase_withstring_test():
     base = IOHandlerBase(['test value 1', 'test value 2'], 'i')
     assert base.io_type == IOType.INPUT
     assert type(base.handler_args) == list
-    assert base.handler_args[0] == 'test value 1'  
+    assert base.handler_args[0] == 'test value 1'
+
+def test_create_iohandlerbase_fails_properly_test():
+    with pytest.raises((CfpValueError, CfpUserInputError)):
+        base = IOHandlerBase(['test value 1', 'test value 2'])
+    with pytest.raises((CfpValueError, CfpUserInputError)):
+        base = IOHandlerBase(['test value 1', 'test value 2'], 'bad input')
+    with pytest.raises((CfpValueError, CfpUserInputError)) as e:
+        base = IOHandlerBase(['test value 1', 'test value 2'], 14)
+    # assert str(e.value) == 'The value provided for io_type must be of type string or IOType.'
 
 def test_create_inputhandler_test():
-    pass
+    hndlr = InputHandler(InputType.INFILE, ['test value 1', 'test value 2'])
+    assert hndlr.input_type == InputType.INFILE
+    assert hndlr.io_type == IOType.INPUT
+    assert hndlr.handler_args[1] == 'test value 2'
