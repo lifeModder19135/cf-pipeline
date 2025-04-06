003860f9174b147c28513d8229e6305b274d0acc
Nathan Tolbert <ntolbertu85@gmail.com>
2025-04-05 21:16:18 -0400

    04032025_--_cfp_errors_test_--_added_cleanup_code:    cfp_contest_test.py:       - added tests:           - test_cfpiniterror_raises_test           - test_cfpioerror_raises_test           - test_cfpconfigurationerror_raises_test           - test_cfpmethodinputerror_raises_test           - test_cfpuserinputerror_raises_test           - test_cfptypeerror_raises_test           - test_cfpvalueerror_raises_test           - test_cfppermissiondeniederror_raises_test           - test_cfpruntimeerror_raises_test           - test_cfpoverwritenotallowederror_raises_test           - test_cfpnotexecutableerror_raises_test           - test_cfptimeouterror_raises_test

--- TEST/modules_test/cfp_errors_test.py
+++ TEST/modules_test/cfp_errors_test.py
@@ -1,1 +1,50 @@
-from SOURCE.modules import cfp_errors
+from SOURCE.modules.cfp_errors import CfpInitializationError, CfpIOError, CfpConfigurationError, CfpMethodInputError, CfpUserInputError, CfpTypeError, CfpValueError, CfpPermissionDeniedError, CfpRuntimeError, CfpOverwriteNotAllowedError, CfpNotExecutableError, CfpTimeoutError
+import pytest
+
+def test_cfpiniterror_raises_test():
+    with pytest.raises(CfpInitializationError):
+        raise CfpInitializationError('test')
+    
+def test_cfpioerror_raises_test():
+    with pytest.raises(CfpIOError):
+        raise CfpIOError('test')
+
+def test_cfpconfigurationerror_raises_test():
+    with pytest.raises(CfpConfigurationError):
+        raise CfpConfigurationError('test')
+    
+def test_cfpmethodinputerror_raises_test():
+    with pytest.raises(CfpMethodInputError):
+        raise CfpMethodInputError('test')
+    
+def test_cfpuserinputerror_raises_test():
+    with pytest.raises(CfpUserInputError):
+        raise CfpUserInputError('test')
+    
+def test_cfptypeerror_raises_test():
+    with pytest.raises(CfpTypeError):
+        raise CfpTypeError('test')
+
+def test_cfpvalueerror_raises_test():
+    with pytest.raises(CfpValueError):
+        raise CfpValueError('test')
+    
+def test_cfppermissiondeniederror_raises_test():
+    with pytest.raises(CfpPermissionDeniedError):
+        raise CfpPermissionDeniedError('test')
+    
+def test_cfpruntimeerror_raises_test():
+    with pytest.raises(CfpRuntimeError):
+        raise CfpRuntimeError('test')
+        
+def test_cfpoverwritenotallowederror_raises_test():
+    with pytest.raises(CfpOverwriteNotAllowedError):
+        raise CfpOverwriteNotAllowedError('test')
+    
+def test_cfpnotexecutableerror_raises_test():
+    with pytest.raises(CfpNotExecutableError):
+        raise CfpNotExecutableError('test')
+        
+def test_cfptimeouterror_raises_test():
+    with pytest.raises(CfpTimeoutError):
+        raise CfpTimeoutError('test')
