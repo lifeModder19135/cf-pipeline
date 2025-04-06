153bfebba10e5b6a22b0c0d1a4f45ba831dd7a56
Nathan Tolbert <ntolbertu85@gmail.com>
2025-04-06 17:23:05 -0400

    04062025_--_cfp_maintutils_test_--_created:    - created file    - added test test_create_mock_conf_file_test

---
+++ TEST/lib_test/libcfp_maintutils_test.py
@@ -0,0 +1,15 @@
+from SOURCE.lib.libcfp_maintutils import create_mock_conf_file, delete_nock_conf_file
+import os
+
+def test_create_mock_conf_file_test():
+    create_mock_conf_file('testdir', 'testfile.txt')
+    line = None
+    path = None
+    if os.name == 'posix' or os.name == 'java':
+        path = 'testdir/testfile.txt'
+    elif os.name == 'nt':
+        path = 'testdir\\testfile.txt'
+    with open(path, 'r') as f:
+        line = f.readlines()[0]
+    assert line == '[[SECTION_1]]\n'
+    delete_nock_conf_file('testdir')
