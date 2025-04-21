4fa8012ff82c3c372bfeffc6113df193edf4326a
Nathan Tolbert <ntolbertu85@gmail.com>
2025-04-20 21:01:47 -0400

    04202025_--_cf_problem_test_--_misc:
        - added changes to tests to mirror changes in tested class

--- TEST/modules_test/cfp_problem_test.py
+++ TEST/modules_test/cfp_problem_test.py
@@ -4,13 +4,13 @@ from SOURCE.modules.cfp_errors import CfpTypeError
 
 def test_create_problem_test():
     tags = ['test', 'test tag', 'test tag 2']
-    pr = Problem(contestId=1, problemset_name='test problemset', index='a', name='test problem', problem_type=ProblemType.CF_PROGRAMMING, points=float(100), rating=100, tags=tags)
+    pr = Problem(contestId=1, problemset_name='test problemset', index='a', name='test problem', type=ProblemType.CF_PROGRAMMING, points=float(100), rating=100, tags=tags)
 
     assert pr.contest_id == 1
     assert pr.index == 'a'
     assert pr.problemset_name == 'test problemset'
     assert pr.name == 'test problem'
-    assert pr.problem_type == ProblemType.CF_PROGRAMMING
+    assert pr.type == ProblemType.CF_PROGRAMMING
     assert pr.points == float(100)
     assert pr.rating == 100
     assert pr.tags == ['test', 'test tag', 'test tag 2']
