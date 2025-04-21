2c678bdadadc0c1232b1a0377c3d2dd1adc8b5fa
Nathan Tolbert <ntolbertu85@gmail.com>
2025-04-20 21:00:01 -0400

    04202025_--_cf_logger_--_misc:
        - added import cfp_errors.CfpTypeError
        - added type-checking to min_level_printed setter
        - added else statement to log method logic
        - added force=true to basicConfig call in init statement

--- SOURCE/modules/cfp_logger.py
+++ SOURCE/modules/cfp_logger.py
@@ -1,5 +1,6 @@
 import logging
 from enum import Flag
+from SOURCE.modules.cfp_errors import CfpTypeError, CfpValueError
 
 class Level(Flag):
     """These values represent different logging levels."""
@@ -20,7 +21,10 @@ class BasicLogger:
     @min_level_printed.setter
     def min_level_printed(self, lvl: Level) -> None:
         """This is the minimum priority level printed to the console."""
-        self.__level = lvl
+        if type(lvl) == Level:
+            self.__level = lvl
+        else:
+            raise CfpTypeError('The value assigned to min_level_printed must be of type Level')
 
     @property
     def filename(self) -> str:
@@ -42,20 +46,22 @@ class BasicLogger:
             logging.error(msg)
         elif level == Level.CRITICAL:
             logging.critical(msg)
+        else:
+            raise CfpValueError
     
 
     def __init__(self, level: Level, logfile: str):
         self.min_level_printed = level
         self.filename = logfile
         if self.min_level_printed == Level.DEBUG:
-            logging.basicConfig(level=logging.DEBUG, filename=self.filename)
+            logging.basicConfig(level=logging.DEBUG, filename=self.filename, filemode='a', force=True)
         elif self.min_level_printed == Level.INFO:
-            logging.basicConfig(level=logging.INFO, filename=self.filename)
+            logging.basicConfig(level=logging.INFO, filename=self.filename, filemode='a', force=True)
         elif self.min_level_printed == Level.WARNING:
-            logging.basicConfig(level=logging.WARNING, filename=self.filename)
+            logging.basicConfig(level=logging.WARNING, filename=self.filename, filemode='a', force=True)
         elif self.min_level_printed == Level.ERROR:
-            logging.basicConfig(level=logging.ERROR, filename=self.filename)
+            logging.basicConfig(level=logging.ERROR, filename=self.filename, filemode='a', force=True)
         elif self.min_level_printed == Level.CRITICAL:
-            logging.basicConfig(level=logging.CRITICAL, filename=self.filename)
+            logging.basicConfig(level=logging.CRITICAL, filename=self.filename, filemode='a', force=True)
 
         
