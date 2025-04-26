5de4c17f5d9303c8d8ff5455fd27a91e3541997d
Nathan Tolbert <ntolbertu85@gmail.com>
2025-04-26 13:30:55 -0400

    04262025_--_cf_testcontext_--_added_class:
        - added class InputParser with 1 method

--- SOURCE/modules/cfp_testcontext.py
+++ SOURCE/modules/cfp_testcontext.py
@@ -1,7 +1,19 @@
 import os, subprocess
+from SOURCE.modules.cfp_context import CfpFile
 
 class simple_tester():
     
     def setup_pipes():
         ps1 = subprocess.run(input=subprocess.STDIN, output=subprocess.PIPE)
 
+class InputParser:
+
+    def parse_input_file(inputfile: CfpFile):
+        path = inputfile.location_path
+        with open(path, 'r') as file:
+            output = {'file_info': [], 'data': []}
+            for line in file.readlines:
+                if line.lstrip().startswith('File:'):
+                    pass
+                elif line.lstrip().startswith('Data:'):
+                    pass
