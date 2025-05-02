4726bbe65be2f47ea35f88d5d92783dac7741f6c
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-02 14:17:32 -0400

    05012025_--_cfp_testcontext_test_--_removed_test:
        - removed test test_fake_test()

--- TEST/modules_test/cfp_testcontext_test.py
+++ TEST/modules_test/cfp_testcontext_test.py
@@ -4,9 +4,4 @@ from pathlib import Path
 def test_input_file_fmt_1_to_input_test(capsys):
     cfp_testcontext.InputParser.input_file_fmt_1_to_input('/home/ntolb/CODING_PROJECTS/python_workspaces/0-vscode_ws/cf-pipeline/TEST/modules_test/testfile.cfpin')
     captured = capsys.readouterr()
-    assert captured.out == '2\n3 3 \n3 3 \n3 3 \n3 3 '
-
-def test_fake_test():
-    cleanline = 'val = 2'
-    res = str(cleanline.split(' ')[2])
-    assert res == '2'
+    assert captured.out == '2\n3 3\n3 3\n3 3\n3 3\n'
