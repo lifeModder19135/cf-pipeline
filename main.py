12f3f57e6be9505cf2f4935fe7cfc89752237539
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-03 03:24:22 -0400

    05022025_--_project_--_misc:
        - deleted duplicate LICENSE file
        - main.py:
            - added to and fixed cf-pipeline command
        - pyproject.toml:
            - added entry point for cf-pipeline command

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import click
 from SOURCE.modules import cfp_context, cfp_config
 
 @click.Group()
-def callcfpcommand(Version:bool=False):
+@click.option('-v', '-V', '--version', is_flag=True, default=False)
+@click.option('-c', '-C', '--contribute', is_flag=True, default=False)
+def callcfpcommand(version, contribute):
     """
     This is the function that is wired to the base cfp command in the terminal, although it is a Group, so it will never be called except to check the version, to get usage info, or in error.
     """
-    if Version:
+    if version and contribute:
+        click.echo('The \'--version\' and \'contribute\' options are mutually exclusive. You can only use one at a time.')
+    if version:
         click.echo("""
             ~~~~~~ CfPipeline -- V-0.0.5  ~~~~~~~~
 
@@ -20,5 +24,27 @@ def callcfpcommand(Version:bool=False):
                 Codename:    "AlmostAlpha"
         """)
         return '0'
+    elif contribute:
+        click.echo("""
+                   Looking for a project to contribute to? This one is looking for 
+                   developers with big ideas. have an idea for a command that would
+                   improve your competitive programming workflow. Build it, add it in 
+                   a github pull request, and it will likely become part of the 
+                   cf-pipeline package. 
+
+                   The cf-pipeline github repo can be found at 
+                   https://github.com/lifeModder19135/cf-pipeline.
+                   All of the source code is available inside the 'SOURCE' directory.
+                   This is a fairly large project with a lot of 'stubs' or functions that are
+                   yet to be built. These are opportunities to add your mark to the project.
+
+                   This project, for me, has been a blast to work on, and so I want to share 
+                   the opportunity with others!
+
+                   For more details on contributing, check out RESOURCES/README.rst in the repo, or 
+                   visit https://github.com/lifeModder19135/cf-pipeline/blob/dev-master/RESOURCES/README.rst.
+                   This document is currently out of date in some areas, but should be updated soon.
+
+        """)
     else:
-        raise ValueError
+        click.echo('Welcome to cf-pipeline!\nTIP: Call this command with \'--help\' option for a list of available options.')
