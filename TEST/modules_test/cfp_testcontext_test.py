68cfff267410549854ea934195e8caaef89a2238
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-08 01:24:22 -0400

    05072025_--_cfp_testcontext_test_--_changed_filename:
        - changed filepath name in test to mirror earlier changes to one of it;s directory names

--- TEST/modules_test/cfp_testcontext_test.py
+++ TEST/modules_test/cfp_testcontext_test.py
@@ -2,6 +2,6 @@ from SOURCE.modules import cfp_testcontext
 from pathlib import Path
 
 def test_input_file_fmt_1_to_input_test(capsys):
-    cfp_testcontext.InputParser.input_file_fmt_1_to_input('./RESOURCES/test-resources/testfile.cfpin')
+    cfp_testcontext.InputParser.input_file_fmt_1_to_input('./RESOURCES/test_resources/testfile.cfpin')
     captured = capsys.readouterr()
     assert captured.out == '2\n3 3\n3 3\n3 3\n3 3\n'
