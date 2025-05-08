157468941d9313281e33e492a18dfd6fc3d31302
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-07 22:42:30 -0400

    05072025_--_SOURCE/commands_--_added_command:
        - added command cf_testproblem
        - added mock files to test command

--- RESOURCES/test_resources/testinput.py
+++ RESOURCES/test_resources/testinput.py
@@ -1,4 +1,5 @@
 from SOURCE.modules.cfp_testcontext import InputParser
+import sys
 
-file = input()
+file = sys.argv[1]
 InputParser.input_file_fmt_1_to_input(file)
--- SOURCE/commands/cf_testproblem.py
+++ SOURCE/commands/cf_testproblem.py
@@ -13,12 +13,56 @@
 
 import click
 from SOURCE.modules.cfp_context import CfpFile, FileType
+from SOURCE.modules.cfp_testcontext import InputParser
 from pathlib import Path
+from subprocess import run
+from os.path import abspath
 
 @click.command()
 @click.argument('solutionfile')
 @click.argument('inputfile')
-def test_solution(inputfile: str, solutionfile: str):
-    if_path = Path(inputfile)
-    sf_path = Path(solutionfile)
-    
+@click.argument('outputfile')
+def test_solution_old(inputfile: str, solutionfile: str, outputfile: str):
+    """This will test a codeforces solution against input provided in the format of a cfpin file. For more details about cfpin files, see cfp_context.py."""
+    abs = str(abspath('./RESOURCES/test_resources/testinput.py'))
+    input = run('python3 -s ' + abs + ' ' + inputfile, capture_output=True, text=True, shell=True) 
+    output = run('python3 -s ' + solutionfile + ' | testoutput.py ' + outputfile, stdin=input.stdout, capture_output=True, text=True, shell=True)
+    exp = InputParser.parse_lines(outputfile)
+    if output.stdout == exp:
+        print('test passed!\n')
+    else:
+        print('test failed')
+    print('output:')
+    print(output.stdout)
+    print('\n')
+    print('expected:')
+    print(exp)
+
+@click.command()
+@click.argument('solutionfile')
+@click.argument('inputfile')
+@click.argument('outputfile')
+def test_solution(inputfile: str, solutionfile: str, outputfile: str):
+    """This will test a codeforces solution against input provided in the format of a cfpin file. For more details about cfpin files, see cfp_context.py."""
+    abs = str(abspath('./RESOURCES/test_resources/testinput.py')) 
+    output = run('python3 -s ' + abs + ' ' + inputfile + ' | python3 -s ' + solutionfile, capture_output=True, text=True, shell=True)
+    exp = InputParser.parse_lines(outputfile)
+    lst = []
+    st = ''
+    for x in output.stdout:
+        
+        if x == '\n':
+            lst.append(str(st))
+            st = ''
+        else:
+            st += x
+    if lst == exp:
+        print('test passed!\n')
+    else:
+        print('test failed')
+    print('output:')
+    print(lst)
+    print('\n')
+    print('expected:')
+    print(exp)
+    print(output.stderr)
