3525cda476122cfb92be20d49410032c591d86bf
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-18 20:29:36 -0400

    05182025_--_cfp_pathutils_--_misc:
        - changed imports
            - changed format of urllib imports
            - added import for webbrowser
        - fixed indentation errors in PathTool class
        - cfp_url:
            - added init method
            - added 3 new methods:
                - open
                - todict
                - construct

--- SOURCE/modules/cfp_pathutils.py
+++ SOURCE/modules/cfp_pathutils.py
@@ -1,23 +1,24 @@
-from urllib import parse
+from urllib.parse import urlparse, urlunparse
+import webbrowser
 
 class PathTool:
     """
     This is a group of functions that will help in manipulating path strings, and in some cases, Path and other <path-like> objects. Right now, it only works on posix paths.
     """
-def slashpath_remove_outermost_prefix(path_str: str):
-    """
-    This removes the outer directory of an input path. It returns the smallest prefix path, the path equivalent to the input if cwd were the next directory in
-    """
-    lstripped: bool = False
-    if path_str.startswith("/"):
-        return ''.join('/', list(map(str, path_str[1:].partition('/'))))
-    else:
-        return list(map(str, path_str.partition('/')))
+    def slashpath_remove_outermost_prefix(path_str: str):
+        """
+        This removes the outer directory of an input path. It returns the smallest prefix path, the path equivalent to the input if cwd were the next directory in
+        """
+        lstripped: bool = False
+        if path_str.startswith("/"):
+            return ''.join('/', list(map(str, path_str[1:].partition('/'))))
+        else:
+            return list(map(str, path_str.partition('/')))
 
-def slashpath_separate_filename_from_pathprefix(path_str: str):
-    """This takes a path and returns both the directory structure and the filename, in that order, in a list."""
-    path_str.rstrip('/')
-    return list(map(str, path_str.rpartition('/')))
+    def slashpath_separate_filename_from_pathprefix(path_str: str):
+        """This takes a path and returns both the directory structure and the filename, in that order, in a list."""
+        path_str.rstrip('/')
+        return list(map(str, path_str.rpartition('/')))
 
 class cfp_url():
     """
@@ -25,5 +26,24 @@ class cfp_url():
     TODO: write this class
     """
     pass
-    # def __init__():
-       
+    def __init__(self, url):
+        self.url_full = url
+        url_p = urlparse(url)
+        self.parseresult = url_p
+        self.scheme = url_p.scheme
+        self.fragment = url_p.fragment
+        self.location = url_p.netloc
+        self.query = url_p.query
+        self.params = url_p.params
+        self.path = url_p.path
+
+    def open(self):
+        webbrowser.open(self.url_full)
+
+    def todict(self):
+        dct = {'scheme': self.scheme, 'base_url': self.location, 'path': self.path, 'query': self.query, 'params': self.params, 'fragment': self.fragment}
+        return dct
+    
+    def construct(self):
+        components = (self.scheme, self.location, self.path, self.params, self.query, self.fragment)
+        return urlunparse(components)
