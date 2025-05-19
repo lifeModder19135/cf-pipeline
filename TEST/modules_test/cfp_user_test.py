f76a3e47ae14146fca72060aa256bac80bcb3db4
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-18 20:45:41 -0400

    05182025_--_cfp_user_test_--_added_tests:
        - fixed args in constructor calls in test_create_user_test and test_user_failsproperly_test
        - added 5 tests:
            - test_user_from_json_test
            - test_user_from_json_response_0objects_test
            - test_user_from_json_response_1object_test
            - test_user_from_json_response_multiple_objects_test
            - test_user_get_blogposts_test (just passes)

--- TEST/modules_test/cfp_user_test.py
+++ TEST/modules_test/cfp_user_test.py
@@ -6,48 +6,49 @@ from pathlib import Path
 def test_user_failsproperly_test():
 
     with raises(CfpTypeError):
-        user = User('test_handle', 
-                    'test@email.com', 
-                    'testid', 
-                    'test_openid', 
-                    'Larry', 
-                    'Smith', 
-                    'USA', 
-                    'New York', 
-                    'TestOrg', 
-                    'test', 
-                    '999', 
-                    999, 
-                    '888', 
-                    888, 
-                    '01010101', 
-                    '23232323', 
-                    123, 
-                    'test avatar', 
-                    '0123456789')
+        user = User(lastName='Smith',
+                    country='USA', 
+                    lastOnlineTimeSeconds=999, 
+                    city='New York', 
+                    rating=888, 
+                    friendOfCount=123,
+                    titlePhoto='/test/path',
+                    handle='test_handle',
+                    avatar='test/path/to/avatar',
+                    firstName='Larry',
+                    contribution=777,
+                    organization='TestOrg',
+                    rank='master',                  
+                    maxRating=888,
+                    registrationTimeSeconds=1010101,
+                    maxRank='master',
+                    email='test@email.com', 
+                    vkId='testid', 
+                    openId='test_openid', 
+                   )
+        user.title_photo = 'not a path'
 
 def test_create_user_test():
-
-    path = Path('/test/path')
-    user = User('test_handle', 
-                'test@email.com', 
-                'testid', 
-                'test_openid', 
-                'Larry', 
-                'Smith', 
-                'USA', 
-                'New York', 
-                'TestOrg', 
-                'test', 
-                '999', 
-                999, 
-                '888', 
-                888, 
-                '01010101', 
-                '23232323', 
-                123, 
-                'test avatar', 
-                path)
+    user = User(lastName='Smith',
+                country='USA', 
+                lastOnlineTimeSeconds=999, 
+                city='New York', 
+                rating=888, 
+                friendOfCount=123,
+                titlePhoto='/test/path',
+                handle='test_handle',
+                avatar='test/path/to/avatar',
+                firstName='Larry',
+                contribution=777,
+                organization='TestOrg',
+                rank='master',                  
+                maxRating=888,
+                registrationTimeSeconds=1010101,
+                maxRank='master',
+                email='test@email.com', 
+                vkId='testid', 
+                openId='test_openid', 
+                )
     assert user.handle == 'test_handle'
     assert user.email == 'test@email.com'
     assert user.vkid == 'testid'
@@ -57,4 +58,32 @@ def test_create_user_test():
     assert user.country == 'USA'
     assert user.city == 'New York'
     assert user.organization == 'TestOrg'
-    assert user.contribution == 'test'
+    assert user.contribution == 777
+
+def test_user_from_json_test():
+    stub = '{"lastName":"Fefer","country":"Russia","lastOnlineTimeSeconds":1672070012,"city":"Saratov","rating":2174,"friendOfCount":415,"titlePhoto":"https://userpic.codeforces.org/242/title/151ab49dee0779f8.jpg","handle":"Fefer_Ivan","avatar":"https://userpic.codeforces.org/242/avatar/c4e6a102a9e66281.jpg","firstName":"Ivan","contribution":0,"organization":"Booking.com","rank":"master","maxRating":2476,"registrationTimeSeconds":1264960450,"maxRank":"grandmaster"}'
+    user = User.from_json(stub)
+    assert user.lastname == 'Fefer'
+    assert user.country == 'Russia'
+
+def test_user_from_json_response_0objects_test():
+    stub = '{"status":"OK","result":[]}'
+    users = User.from_json_response(stub)
+    assert users == None
+
+def test_user_from_json_response_1object_test():
+    stub = '{"status":"OK","result":[{"lastName":"Fefer","country":"Russia","lastOnlineTimeSeconds":1672070012,"city":"Saratov","rating":2174,"friendOfCount":415,"titlePhoto":"https://userpic.codeforces.org/242/title/151ab49dee0779f8.jpg","handle":"Fefer_Ivan","avatar":"https://userpic.codeforces.org/242/avatar/c4e6a102a9e66281.jpg","firstName":"Ivan","contribution":0,"organization":"Booking.com","rank":"master","maxRating":2476,"registrationTimeSeconds":1264960450,"maxRank":"grandmaster"}]}'
+    user = User.from_json_response(stub)
+    assert type(user) == User
+    assert user.firstname == 'Ivan'
+
+def test_user_from_json_response_multiple_objects_test():
+    stub = '{"status":"OK","result":[{"lastName":"Khodyrev","lastOnlineTimeSeconds":1742481459,"rating":1709,"friendOfCount":95,"titlePhoto":"https://userpic.codeforces.org/1592/title/27e43714e4bea090.jpg","handle":"DmitriyH","avatar":"https://userpic.codeforces.org/1592/avatar/7cef566902732053.jpg","firstName":"Dmitriy","contribution":0,"organization":"","rank":"expert","maxRating":2072,"registrationTimeSeconds":1268570311,"maxRank":"candidate master"},{"lastName":"Fefer","country":"Russia","lastOnlineTimeSeconds":1672070012,"city":"Saratov","rating":2174,"friendOfCount":415,"titlePhoto":"https://userpic.codeforces.org/242/title/151ab49dee0779f8.jpg","handle":"Fefer_Ivan","avatar":"https://userpic.codeforces.org/242/avatar/c4e6a102a9e66281.jpg","firstName":"Ivan","contribution":0,"organization":"Booking.com","rank":"master","maxRating":2476,"registrationTimeSeconds":1264960450,"maxRank":"grandmaster"}]}'
+    users = User.from_json_response(stub)
+    assert type(users) == list
+    assert users[0].firstname == 'Dmitriy'
+    assert users[1].firstname == 'Ivan'
+
+def test_user_get_blogposts_test():
+    pass
+
