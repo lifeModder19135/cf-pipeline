1e9bc99576bdbfa46079c127e714d859fd02677d
Nathan Tolbert <ntolbertu85@gmail.com>
2025-05-31 01:38:37 -0400

    05302025_--_cfp_pathutils_--_fixed_methods:
        - cfp_pathutils:
            - added imports:
                - os
                - re
            - fixed method slashpath_remove_outermost_prefix()
            - fixed method slashpath_separate_filename_from_pathprefix()
            - changed class cfp_url to more pythonic CfpUrl
        - cfp_pathutils_test:
            - added 8 tests:
                - test_pathtool_slashpath_remove_outermost_prefix_linux_abs_test()
                - test_pathtool_slashpath_remove_outermost_prefix_linux_rel_test()
                - test_pathtool_slashpath_remove_outermost_prefix_windows_abs_test
                - test_pathtool_slashpath_remove_outermost_prefix_windows_rel_test
                - test_slashpath_separate_filename_from_pathprefix_linux_file_test
                - test_slashpath_separate_filename_from_pathprefix_linux_dir_test
                - test_slashpath_separate_filename_from_pathprefix_windows_dir_test
                - test_slashpath_separate_filename_from_pathprefix_windows_file_test
            - all tests pass

--- SOURCE/modules/cfp_pathutils.py
+++ SOURCE/modules/cfp_pathutils.py
@@ -1,26 +1,59 @@
 from urllib.parse import urlparse, urlunparse
 import webbrowser
+import os
+import re
 
 class PathTool:
     """
     This is a group of functions that will help in manipulating path strings, and in some cases, Path and other <path-like> objects. Right now, it only works on posix paths.
     """
+
+    @staticmethod
     def slashpath_remove_outermost_prefix(path_str: str):
         """
         This removes the outer directory of an input path. It returns the smallest prefix path, the path equivalent to the input if cwd were the next directory in
         """
-        lstripped: bool = False
-        if path_str.startswith("/"):
-            return ''.join('/', list(map(str, path_str[1:].partition('/'))))
+        if os.name == 'posix':
+            sep = '/'
         else:
-            return list(map(str, path_str.partition('/')))
+            sep = '\\'
 
+        if path_str.startswith("/"):
+            path_str = path_str[1:]
+        elif bool(re.match('^[A-Z]:\\\\', path_str)):
+            path_str = path_str[3:]
+
+        path_lst = path_str.split(sep)
+        new_path_lst = path_lst[1:]
+        return sep.join(new_path_lst)
+
+    @staticmethod
     def slashpath_separate_filename_from_pathprefix(path_str: str):
-        """This takes a path and returns both the directory structure and the filename, in that order, in a list."""
-        path_str.rstrip('/')
-        return list(map(str, path_str.rpartition('/')))
+        """This takes a path and returns the directory structure, the separator, and the filename, in that order, in a list. Alternately, if the given path points to a directory, it will break off that last directory and return path-to-directory, separator, and directory in a list in that order."""
+        isdir = False
+        isabs = False
+        if os.name == 'posix':
+            sep = '/'
+        else:
+            sep = '\\'
 
-class cfp_url():
+        if path_str.endswith(sep):
+            isdir = True
+
+        if isdir == False:
+            return list(map(str, path_str.rpartition(sep)))
+        else:
+            new_path = path_str.rstrip(sep)
+            lst = new_path.split(sep)
+            path_dir = lst[-1]
+            prefix = sep.join(lst[:-1])
+
+            result = [prefix, sep, path_dir]
+            return result
+
+        
+
+class CfpUrl():
     """
     Not yet implemented. This will be a wrapper object for the named tuple returned by urllib parse function, which  takes in a url string and splits it into six sections: the protocol, net/host, params, path, attributes, and values
     TODO: write this class
@@ -47,3 +80,6 @@ class cfp_url():
     def construct(self):
         components = (self.scheme, self.location, self.path, self.params, self.query, self.fragment)
         return urlunparse(components)
+    
+if __name__ == '__main__':
+    res = PathTool.slashpath_separate_filename_from_pathprefix('C:\\home\\user\\download1.exe')
--- TEST/modules_test/cfp_pathutils_test.py
+++ TEST/modules_test/cfp_pathutils_test.py
@@ -1,1 +1,76 @@
-from SOURCE.modules import cfp_pathutils
+from SOURCE.modules.cfp_pathutils import PathTool, CfpUrl
+import os
+
+
+def test_pathtool_slashpath_remove_outermost_prefix_linux_abs_test():
+    realos = os.name
+    os.name = 'posix'
+    path = '/home/user/downloads'
+    new_path = PathTool.slashpath_remove_outermost_prefix(path)
+    assert new_path == 'user/downloads'
+    os.name = realos
+
+def test_pathtool_slashpath_remove_outermost_prefix_linux_rel_test():
+    realos = os.name
+    os.name = 'posix'
+    path = 'user/downloads/download1'
+    new_path = PathTool.slashpath_remove_outermost_prefix(path)
+    assert new_path == 'downloads/download1'
+    os.name = realos
+
+def test_pathtool_slashpath_remove_outermost_prefix_windows_abs_test():
+    realos = os.name
+    os.name = 'windows'
+    path = r'C:\users\user\home'
+    new_path = PathTool.slashpath_remove_outermost_prefix(path)
+    assert new_path == r'user\home'
+    os.name = realos
+
+def test_pathtool_slashpath_remove_outermost_prefix_windows_rel_test():
+    realos = os.name
+    os.name = 'windows'
+    path = r'users\user\home'
+    new_path = PathTool.slashpath_remove_outermost_prefix(path)
+    assert new_path == r'user\home'
+    os.name = realos
+
+def test_slashpath_separate_filename_from_pathprefix_linux_file_test():
+    realos = os.name
+    os.name = 'posix'
+    path = '/home/user/download.file'
+    path_list = PathTool.slashpath_separate_filename_from_pathprefix(path)
+    assert path_list[0] == '/home/user'
+    assert path_list[1] == '/'
+    assert path_list[2] == 'download.file'
+    os.name = realos
+
+def test_slashpath_separate_filename_from_pathprefix_linux_dir_test():
+    realos = os.name
+    os.name = 'posix'
+    path = '/home/user/downloads/'
+    path_list = PathTool.slashpath_separate_filename_from_pathprefix(path)
+    assert path_list[0] == '/home/user'
+    assert path_list[1] == '/'
+    assert path_list[2] == 'downloads'
+    os.name = realos
+
+def test_slashpath_separate_filename_from_pathprefix_windows_dir_test():
+    realos = os.name
+    os.name = 'windows'
+    path = 'C:\\home\\user\\downloads\\'
+    path_list = PathTool.slashpath_separate_filename_from_pathprefix(path)
+    assert path_list[0] == 'C:\\home\\user'
+    assert path_list[1] == '\\'
+    assert path_list[2] == 'downloads'
+    os.name = realos
+
+def test_slashpath_separate_filename_from_pathprefix_windows_file_test():
+    realos = os.name
+    os.name = 'windows'
+    path = 'C:\\home\\user\\download1.exe'
+    path_list = PathTool.slashpath_separate_filename_from_pathprefix(path)
+    assert path_list[0] == 'C:\\home\\user'
+    assert path_list[1] == '\\'
+    assert path_list[2] == 'download1.exe'
+    os.name = realos
+
