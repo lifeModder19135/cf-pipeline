df80cdf909806ee7b7f719d4584bb1bcf2fe63de
Nathan Tolbert <ntolbertu85@gmail.com>
2025-06-01 18:28:54 -0400

    06012025_--_cfp_config_test_--_added tests:
        - imports:
            - removed SOURCE.modules.cfp_config.Action
            - added 3:
                - SOURCE.modules.cfp_errors.CfpMethodInputError
                - os
                - pytest
        - changed name of test_create_configfile_test() to test_create_configuration_test()
        - added 7 tests:
            - test_configuration__get_slash_type_linux_test()
            - test_configuration__get_slash_type_windows_test()
            - test_configuration_create_config_file_doesnt_exist_test()
            - test_configuration_create_config_file_exists_test()
            - test_configuration_add_section_to_config_file_test()
            - test_configuration_add_section_to_config_file_fails_correctly_test()
            - test_configuration_get_section_from_config_file_test()

--- TEST/modules_test/cfp_config_test.py
+++ TEST/modules_test/cfp_config_test.py
@@ -1,4 +1,7 @@
-from SOURCE.modules.cfp_config import ConfigSection, Configuration, Action
+from SOURCE.modules.cfp_config import ConfigSection, Configuration
+from SOURCE.modules.cfp_errors import CfpMethodInputError
+import os
+import pytest
 
 def test_create_configsection_test():
 
@@ -13,11 +16,83 @@ def test_update_configsection_test():
     section.update({'test_key_2': 'test_value_2'})
     assert section.values['test_key_2'] == 'test_value_2'
 
-def test_create_configfile_test():
+def test_create_configuration_test():
     section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
     section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
-    file = Configuration('/test/path', 'test.py', [section_1, section_2])
-    assert file.location_dirpath == '/test/path'
-    assert file.filename == 'test.py'
-    assert type(file.sections[0]) == ConfigSection
-    assert type(file.sections[1]) == ConfigSection
+    conf = Configuration('/test/path', 'test.py', [section_1, section_2])
+    assert conf.location_dirpath == '/test/path'
+    assert conf.filename == 'test.py'
+    assert type(conf.sections[0]) == ConfigSection
+    assert type(conf.sections[1]) == ConfigSection
+
+def test_configuration__get_slash_type_linux_test(monkeypatch):
+    monkeypatch.setattr(os, 'name', 'posix')
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration('/test/path', 'test.py', [section_1, section_2])
+    slash = conf._Configuration__get_slash_type()
+    assert slash == '/'
+
+def test_configuration__get_slash_type_windows_test(monkeypatch):
+    monkeypatch.setattr(os, 'name', 'nt')
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration('/test/path', 'test.py', [section_1, section_2])
+    slash = conf._Configuration__get_slash_type()
+    assert slash == '\\'
+
+def test_configuration_create_config_file_doesnt_exist_test():
+    fullpath = os.path.abspath('RESOURCES/test_resources/test_config_file_2')
+    if os.path.exists(fullpath):
+        os.remove(fullpath)
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration(os.path.abspath('RESOURCES/test_resources/'), 'test_config_file_2', [section_1, section_2])
+    exists = conf.create_config_file()
+    assert exists == True
+    assert os.path.exists(fullpath)
+
+def test_configuration_create_config_file_exists_test():
+    fullpath = os.path.abspath('RESOURCES/test_resources/test_config_file_2')
+    if not os.path.exists(fullpath):
+        with open(fullpath, 'x'):
+            pass
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration(os.path.abspath('RESOURCES/test_resources/'), 'test_config_file_2', [section_1, section_2])
+    exists = conf.create_config_file()
+    assert exists == True
+    assert os.path.exists(fullpath)
+
+def test_configuration_add_section_to_config_file_test():
+    fullpath = os.path.abspath('RESOURCES/test_resources/test_config_file_2')
+    if not os.path.exists(fullpath):
+        with open(fullpath, 'x'):
+            pass
+    secname = 'test_section'
+    valslist = ['key1 = val1', 'key2 = val2']
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration(os.path.abspath('RESOURCES/test_resources/'), 'test_config_file_2', [section_1, section_2])
+    written = conf.add_section_to_config_file(secname, valslist)
+    assert written == True
+    with open(fullpath, 'r') as file:
+        assert file.readline() == '[[test_section]]\n'
+    os.remove(fullpath)
+
+def test_configuration_add_section_to_config_file_fails_correctly_test():
+    fullpath = os.path.abspath('RESOURCES/test_resources/test_config_file_2')
+    if not os.path.exists(fullpath):
+        with open(fullpath, 'x'):
+            pass
+    secname = 'test_section'
+    valslist = ['key1 = val1', 'key2 = val2', 'bad value']
+    section_1 = ConfigSection('test section 1', 'a test section', {'test_key': 'test_value'})
+    section_2 = ConfigSection('test section 2', 'a test section', {'test_key': 'test_value'})
+    conf = Configuration(os.path.abspath('RESOURCES/test_resources/'), 'test_config_file_2', [section_1, section_2])
+    with pytest.raises(CfpMethodInputError):
+        written = conf.add_section_to_config_file(secname, valslist)
+    os.remove(fullpath)
+
+def test_configuration_get_section_from_config_file_test():
+    pass
