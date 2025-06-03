2388a5210931c2322a28a06ba58cb2803dde0ffa
Nathan Tolbert <ntolbertu85@gmail.com>
2025-06-03 00:59:25 -0400

    06022025_--_cfp_session_test_--_misc:
        - added import os
        - added convenience function get_slash()
        - used get_slash() to replace boilerplate code in test_create_cfsession_test()
        - added code to remove config file at cleanup in test_create_cfsession_test()

--- TEST/modules_test/cfp_session_test.py
+++ TEST/modules_test/cfp_session_test.py
@@ -1,13 +1,24 @@
 from SOURCE.modules.cfp_session import CfSession
 from SOURCE.modules.cfp_config import ConfigSection, Configuration
+import os
 
+
+def get_slash():
+    if os.name == 'posix' or os.name == 'java':
+        return '/'
+    else:
+        return '\\'
 
 def test_create_cfsession_test():
-    sect = ConfigSection('section1', 'section 1', {'key1': 'value 1'})
-    conf = Configuration('/test/path', 'test.py', [sect])
+    slash = get_slash()
+    dirpath = os.path.abspath('RESOURCES' + slash + 'test_resources')
+    conf = Configuration(dirpath, 'test_config_file_2', [])
     ses = CfSession('uname', 'password', '1234567890', '0987654321', conf)
     assert ses.username == 'uname'
     assert ses.password == 'password'
     assert ses.api_key == '1234567890'
     assert ses.secret == '0987654321'
     assert ses.config == conf
+    fullpath = dirpath + slash + 'test_config_file_2'
+    if os.path.exists(fullpath):
+        os.remove(fullpath)
